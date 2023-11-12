313e66bcfeba3f63723f5b820057c9579fba8156
Mahmudul_Hasan_Mridul <40644429+m-h-mridul@users.noreply.github.com>
2023-11-12 16:42:38 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+In this web application i try ci/cd pipelines with apache web server,github webhock, Amazon web Cloud (VPC,Ec2). For excess this application only open those port which are require.
+Here the application is only frontend application. 
+
+step 1: Goto cloud provider system here i am using aws for deploying my web application.
+g
---
+++ code.sh
@@ -0,0 +1,55 @@
+
+echo " install jenkins ----"
+
+
+sudo apt update
+
+sudo apt install apache2 -y 
+
+sudo systemctl start apache2
+
+sudo systemctl enable apache2 
+
+sudo systemctl start apache2
+
+sudo apt install openjdk-11-jre -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list >/dev/null
+
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+sudo systemctl restart jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+sudo usermod -aG  jenkins $USER
+
+sudo chown jenkins:www-data html
+
+sudo usermod -aG www-data jenkins
+
+echo "Working started"
+
+rm -rf /var/www/html/*
+cp -r * /var/www/html/
+
+
+
+
+
+
+
+
+AlgoriThm1996!@#
+
+
